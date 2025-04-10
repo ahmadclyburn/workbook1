14f36097e110e8dc0c4d4212be5d3cea3ca2fe50
Ahmad Clyburn <ahmadclyburn61@gmail.com>
2025-04-10 10:24:32 -0400

    completed shopping list app

---
+++ com.pluralsight/src/shoppinglist.java
@@ -0,0 +1,4 @@
+public class shoppinglist {
+    public static void main(String[] args) {
+        System.out.println("yogurt, chicken breast, granola, banana, cheese, ground beef, lettuce, tomato, fettucine, mozzerella");}
+}
