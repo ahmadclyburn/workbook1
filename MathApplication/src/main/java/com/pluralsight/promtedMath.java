577e8f982158bd6e69a6e29999fc9d558b7c4ef4
Ahmad Clyburn <ahmadclyburn61@gmail.com>
2025-04-11 15:53:09 -0400

    prompted math application

---
+++ MathApplication/src/main/java/com/pluralsight/promtedMath.java
@@ -0,0 +1,33 @@
+package com.pluralsight;
+import javax.swing.*;
+import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
+import java.util.*;
+public class promtedMath {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("just give the first num ");
+        int firstNum = scanner.nextInt();
+
+        System.out.println("whats your second ");
+        int secondNum = scanner.nextInt();
+
+        System.out.println("now pick one or get out of my face ");
+        System.out.println("m-");
+        System.out.println("d-");
+        System.out.println("a-");
+        System.out.println("s-");
+        scanner.nextLine();
+        String action = scanner.nextLine();
+
+        if (action.equals("m")) {
+            System.out.println("the classics never die do they. here ya go " + (firstNum * secondNum));
+        }else if(action.equals("d")) {
+            System.out.println("really a big fan of counter culture huh "+ (firstNum / secondNum));
+        }else if(action.equals("a")) {
+            System.out.println("simple as always i see "+ (firstNum + secondNum));
+        }else if(action.equals("s")){
+            System.out.println("daring choice i guess. your answer is " + (firstNum - secondNum));
+                    }
+                }
+    }
